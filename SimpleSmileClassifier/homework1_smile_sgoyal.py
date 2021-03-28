4422b51a654c3beb89a8f950ba84b3a48e97278f
Suryanshg <Suryanshg47@gmail.com>
2021-03-28 13:39:30 -0400

    Added code for measuring accuracy of ensemble predictors

--- SimpleSmileClassifier/homework1_smile_sgoyal.py
+++ SimpleSmileClassifier/homework1_smile_sgoyal.py
@@ -8,7 +8,25 @@ def fPC (y, yhat):
 
 # Returns the Ensemble Accuracy of predictors 
 def measureAccuracyOfPredictors (predictors, X, y):
-    return 1
+    sum_y = np.zeros(y.shape) # Initialize empty array for storing the sum
+    for predictor in predictors:
+        r1,c1,r2,c2 = predictor
+        feature = X[:,r1,c1] - X[:,r2,c2] # Obtain a feature for a particular predictor
+
+        # Performing indicator function a single feature
+        feature[feature<=0] = 0 # if value is negative or zero -> 0
+        feature[feature>0] = 1 # if value is positive -> 1
+
+        sum_y += feature
+    ensemble_y = sum_y / len(predictors)
+
+    # Performing indicator function for the ensemble feature
+    ensemble_y[ensemble_y <= 0.5] = 0
+    ensemble_y[ensemble_y > 0.5] = 1
+
+    accuracy = fPC(y,ensemble_y)
+        
+    return accuracy
 
 # Performs Step-wise Classification on the training data for a feature set of 5 elements
 def stepwiseRegression (trainingFaces, trainingLabels, testingFaces, testingLabels):
@@ -16,12 +34,12 @@ def stepwiseRegression (trainingFaces, trainingLabels, testingFaces, testingLabe
     for j in range(5):
         bestAccuracy = 0
         bestFeature = (0,0,0,0)
-        for r1 in range(5): # Compares all possible set of pixels 
-            for c1 in range(5):
-                for r2 in range(5):
-                    for c2 in range(5):
+        for r1 in range(24): # Compares all possible set of pixels 
+            for c1 in range(24):
+                for r2 in range(24):
+                    for c2 in range(24):
                         if((r1,c1)!=(r2,c2)): # If both the pixels are different on the basis of location
-                            if((r1,c1,r2,c2) not in predictors): # And the current feature is already not selected as a "Best feature"
+                            # if((r1,c1,r2,c2) not in predictors): # And the current feature is already not selected as a "Best feature"
                                 currentAccuracy = measureAccuracyOfPredictors(predictors + [(r1,c1,r2,c2)], trainingFaces, trainingLabels)
                                 if currentAccuracy > bestAccuracy: # if we achieve a best accuracy so far
                                     bestAccuracy = currentAccuracy
@@ -57,8 +75,12 @@ if __name__ == "__main__":
     testingFaces, testingLabels = loadData("test")
     trainingFaces, trainingLabels = loadData("train")
     # print(trainingFaces.shape)
-    predictors = stepwiseRegression(trainingFaces, trainingLabels, testingFaces, testingLabels)
+
+    predictors = stepwiseRegression(trainingFaces[:400], trainingLabels[:400], testingFaces, testingLabels)
     print(predictors)
+    print("Accuracy: ",measureAccuracyOfPredictors(predictors, trainingFaces, trainingLabels))
+
     # y = np.array([1,0,0,1,1])
     # yhat = np.array([0,0,0,0,1])
     # print(fPC(y,yhat))
+
