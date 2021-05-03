f1cde84832f4e82724a27e8a9dcf9231837ffa4b
Suryanshg <Suryanshg47@gmail.com>
2021-05-03 01:19:58 -0400

    Good Night ANN

--- ANN/homework6_sgoyal.py
+++ ANN/homework6_sgoyal.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize
+import math
 
 NUM_INPUT = 784  # Number of input neurons
 NUM_HIDDEN = 40  # Number of hidden neurons
@@ -20,9 +21,10 @@ def relu(z):
 
 # Softmax Activation function, z = (10,n)
 def softmax(z):
-    A = np.exp(z)
-    B = np.sum(A, axis = 0)
-    return A/B
+    zT = z.T # (n,10)
+    # A = np.exp(z)
+    # B = np.sum(A, axis = 0)
+    return (np.exp(zT)/(np.sum(np.exp(zT), axis = 1).reshape(len(zT),1))).T
 
 
 # Relu prime activation on the input, z = (40,n)
@@ -34,13 +36,14 @@ def reluprime(z):
 # Given a vector w containing all the weights and biased vectors, extract
 # and return the individual weights and biases W1, b1, W2, b2.
 # This is useful for performing a gradient check with check_grad.
-def unpack (w):
-    endW1 = NUM_HIDDEN * NUM_INPUT
-    endb1 = endW1 + NUM_HIDDEN
-    endW2 = endb1 + (NUM_OUTPUT * NUM_HIDDEN)
-    W1 = w[:endW1].reshape(NUM_HIDDEN, NUM_INPUT)
-    b1 = w[endW1:endb1].reshape(NUM_HIDDEN)
-    W2 = w[endb1:endW2].reshape(NUM_OUTPUT, NUM_HIDDEN)
+# TODO: add param hidden units
+def unpack (w, hiddenUnits):
+    endW1 = hiddenUnits * NUM_INPUT
+    endb1 = endW1 + hiddenUnits
+    endW2 = endb1 + (NUM_OUTPUT * hiddenUnits)
+    W1 = w[:endW1].reshape(hiddenUnits, NUM_INPUT)
+    b1 = w[endW1:endb1].reshape(hiddenUnits)
+    W2 = w[endb1:endW2].reshape(NUM_OUTPUT, hiddenUnits)
     b2 = w[endW2:].reshape(NUM_OUTPUT)
     return W1, b1, W2, b2
 
@@ -64,11 +67,13 @@ def fPC(y,yhat):
     return np.mean(labelY == labelYhat)
 
 # Calculates yhat for a given data and weights and biases associated
-def getYHat(X,w):
+# TODO: add param hidden units, recheck
+def getYHat(X,w,hiddenUnits):
     # Shape of X is (n,784)
-    W1, b1, W2, b2 = unpack(w)
+    # TODO: add arg hidden units
+    W1, b1, W2, b2 = unpack(w, hiddenUnits)
 
-    b1 = b1.reshape(NUM_HIDDEN,1)
+    b1 = b1.reshape(hiddenUnits,1)
     b2 = b2.reshape(NUM_OUTPUT,1)
 
     # Calculating according to the equations
@@ -83,8 +88,8 @@ def getYHat(X,w):
 # and bias terms w, compute and return the cross-entropy (CE) loss. You might
 # want to extend this function to return multiple arguments (in which case you
 # will also need to modify slightly the gradient check code below).
-def fCE (X, Y, w):
-    yhat = getYHat(X,w) # (n,10)
+def fCE (X, Y, w, hiddenUnits):
+    yhat = getYHat(X,w,hiddenUnits) # (n,10)
     cost = -np.sum(Y * np.log(yhat))/len(Y)
     return cost
 
@@ -92,11 +97,15 @@ def fCE (X, Y, w):
 # and bias terms w, compute and return the gradient of fCE. You might
 # want to extend this function to return multiple arguments (in which case you
 # will also need to modify slightly the gradient check code below).
-def gradCE (X, Y, w):
-    W1, b1, W2, b2 = unpack(w)
+# TODO: add param hidden units
+def gradCE (X, Y, w, hiddenUnits):
 
-    b1 = b1.reshape(NUM_HIDDEN,1)
-    b2 = b2.reshape(NUM_OUTPUT,1)
+    # TODO: add arg hidden units
+    W1, b1, W2, b2 = unpack(w,hiddenUnits)
+
+    # X here is (784,n)
+    b1 = b1.reshape(hiddenUnits,1) # (40,n)
+    b2 = b2.reshape(NUM_OUTPUT,1) # (10,n)
 
     # Performing foward propagation according to the equations
     z1 = W1.dot(X) + b1 # (40,n)
@@ -113,35 +122,104 @@ def gradCE (X, Y, w):
     g = gT.T # (40,n)
 
     gradW1 = g.dot(X.T)/Y.shape[0] # (40,784)
-    gradb1 =  np.average(g, axis = 1).reshape(NUM_HIDDEN,1) # (40,1)
+    gradb1 =  np.average(g, axis = 1).reshape(hiddenUnits,1) # (40,1)
 
     grad = pack(gradW1,gradb1,gradW2,gradb2)
     return grad
 
+# Randomly shuffles the data
+def randomizeData(X,y):
+    n = len(y)
+    randomizedIndex = np.random.permutation(n)
+    return X.T[randomizedIndex].T,y[randomizedIndex]
+
 # Given training and testing datasets and an initial set of weights/biases b,
 # train the NN.
-def train (trainX, trainY, testX, testY, w, epsilon, miniBatchSize, epochs, alpha):
-    W1,b1,W2,b2 = unpack(w)
-    b1 = b1.reshape(NUM_HIDDEN,1)
-    b2 = b2.reshape(NUM_OUTPUT,1)
-    pass
+# TODO: replace w param with hidden units
+def train(trainX, trainY, hiddenUnits, epsilon, miniBatchSize, epochs, alpha):
+    # trainX is (784,60000)
+
+    # Initialize weights randomly
+    W1 = 2*(np.random.random(size=(hiddenUnits, NUM_INPUT))/NUM_INPUT**0.5) - 1./NUM_INPUT**0.5 # (40,784)
+    b1 = 0.01 * np.ones(hiddenUnits) # (40,)
+    W2 = 2*(np.random.random(size=(NUM_OUTPUT, hiddenUnits))/hiddenUnits**0.5) - 1./hiddenUnits**0.5 # (10,40)
+    b2 = 0.01 * np.ones(NUM_OUTPUT) # (10,)
+    
+    w = pack(W1,b1,W2,b2)
+
+    N = len(trainY)
+    batches = math.ceil(N/miniBatchSize)
+    
+    X,y = randomizeData(trainX,trainY)
+    for epoch in range(epochs):
+        for j in range(batches):
+            startIndex = j * miniBatchSize
+            endIndex = (j * miniBatchSize) + miniBatchSize
+
+            miniX = X.T[startIndex:endIndex].T
+            miniY = y[startIndex:endIndex]
+
+            # Extracting Weights and biases
+            # TODO: add arg hidden units
+            W1,b1,W2,b2 = unpack(w, hiddenUnits)
+            b1 = b1.reshape(hiddenUnits,1)
+            b2 = b2.reshape(NUM_OUTPUT,1)
+
+            grad = gradCE(miniX,miniY,w,hiddenUnits)
+
+            # TODO: add arg hidden units
+            gradW1, gradb1, gradW2, gradb2 = unpack(grad, hiddenUnits)
+
+            gradb1 = gradb1.reshape(hiddenUnits,1) # (40,1)
+            gradb2 = gradb2.reshape(NUM_OUTPUT,1) # (10,1)
+            
+           
+            # Updating the weights and biases
+            W1 = W1 - epsilon * (gradW1 - ((alpha * W1)/len(miniY)))
+            b1 = b1 - epsilon * (gradb1 - ((alpha * b1)/len(miniY)))
+            W2 = W2 - epsilon * (gradW2 - ((alpha * W2)/len(miniY)))
+            b2 = b2 - epsilon * (gradb2 - ((alpha * b2)/len(miniY)))
+
+            w = pack(W1,b1,W2,b2)
+
+    return w
 
 # Optimize the hyperparameters
 def findBestHyperparameters(trainX, trainY, valX, valY):
-    bestNumUnitsInHiddenLayer = 0 # Keep Default
+    bestNumUnitsInHiddenLayer = 0 
     bestEpsilon = 0
     bestMiniBatchSize = 0
     bestNumEpochs = 0
-    bestAlpha = 0 # Keep 0.01
+    # bestAlpha = 0 # Keep 0.01
     bestAcc = 0
     
-    # unitsInHiddenLayer = [30,40,50]
+    unitsInHiddenLayer = [30,40,50]
     epochs = [20, 30, 40]
     epsilons = [0.01, 0.05]
-    miniBatchSizes = [32, 64]
-    alphas = [0.01]
+    miniBatchSizes = [32, 64, 128, 256]
+    alpha = 0.01
 
-    return bestNumUnitsInHiddenLayer, bestEpsilon, bestMiniBatchSize, bestNumEpochs, bestAlpha
+    for hiddenUnits in unitsInHiddenLayer:
+        for epsilon in epsilons:
+            for miniBatchSize in miniBatchSizes:
+                for epoch in epochs:
+
+                    # TODO: replace arg w with hidden units
+                    trainedW = train(trainX, trainY, hiddenUnits, epsilon, miniBatchSize, epoch, alpha)
+                    valYHat = getYHat(valX, trainedW, hiddenUnits)
+
+                    curAcc = fPC(valY, valYHat)
+                    print("hiddenUnits: {}, epsilon: {}, batchSize: {}, epochs: {}, alpha: {}, accuracy: {}".format(hiddenUnits, epsilon, miniBatchSize, epoch, alpha, curAcc))
+
+                    if curAcc > bestAcc:
+                        bestAcc = curAcc
+                        bestNumUnitsInHiddenLayer = hiddenUnits
+                        bestEpsilon = epsilon
+                        bestMiniBatchSize = miniBatchSize
+                        bestNumEpochs = epoch
+                        
+
+    return  bestNumUnitsInHiddenLayer,bestEpsilon, bestMiniBatchSize, bestNumEpochs, alpha
 
 # Randomly shuffles the data and returns 20% of that as validation data
 def getValidationData(X,y):
@@ -164,34 +242,51 @@ if __name__ == "__main__":
     testX = testX.T/255
     valX = valX.T/255
 
-
     trainY = getOneHotVectors(trainY)
     testY = getOneHotVectors(testY)
     valY = getOneHotVectors(valY)
-
+    
     # Initialize weights randomly
-    W1 = 2*(np.random.random(size=(NUM_HIDDEN, NUM_INPUT))/NUM_INPUT**0.5) - 1./NUM_INPUT**0.5 # (40,784)
-    b1 = 0.01 * np.ones(NUM_HIDDEN) # (40,)
-    W2 = 2*(np.random.random(size=(NUM_OUTPUT, NUM_HIDDEN))/NUM_HIDDEN**0.5) - 1./NUM_HIDDEN**0.5 # (10,40)
-    b2 = 0.01 * np.ones(NUM_OUTPUT) # (10,)
+    # W1 = 2*(np.random.random(size=(NUM_HIDDEN, NUM_INPUT))/NUM_INPUT**0.5) - 1./NUM_INPUT**0.5 # (40,784)
+    # b1 = 0.01 * np.ones(NUM_HIDDEN) # (40,)
+    # W2 = 2*(np.random.random(size=(NUM_OUTPUT, NUM_HIDDEN))/NUM_HIDDEN**0.5) - 1./NUM_HIDDEN**0.5 # (10,40)
+    # b2 = 0.01 * np.ones(NUM_OUTPUT) # (10,)
 
+    # W1 = 2*(np.random.random(size=(30, NUM_INPUT))/NUM_INPUT**0.5) - 1./NUM_INPUT**0.5 # (30,784)
+    # b1 = 0.01 * np.ones(30) # (30,)
+    # W2 = 2*(np.random.random(size=(NUM_OUTPUT, 30))/30**0.5) - 1./30**0.5 # (10,30)
+    # b2 = 0.01 * np.ones(NUM_OUTPUT) # (10,)
+    
+
+    
     # Concatenate all the weights and biases into one vector; this is necessary for check_grad
-    w = pack(W1, b1, W2, b2)
-
-    # z = np.array([[1,2,3],[-1,-2,-3],[0,0,-1]])
-    # print(softmax(z))
-    print(fCE(trainX, trainY, w))
+    # w = pack(W1, b1, W2, b2)
     
-    
+    '''
     # Check that the gradient is correct on just a few examples (randomly drawn).
     idxs = np.random.permutation(trainX.shape[0])[0:NUM_CHECK]
     print(scipy.optimize.check_grad(lambda w_: fCE(np.atleast_2d(trainX[:,idxs]), np.atleast_2d(trainY[idxs,:]), w_), \
                                     lambda w_: gradCE(np.atleast_2d(trainX[:,idxs]), np.atleast_2d(trainY[idxs,:]), w_), \
                                     w))
+    '''
     
     
     # Hyperparameter Tuning
+    print()
+    bestNumUnitsInHiddenLayer, bestEpsilon, bestMiniBatchSize, bestNumEpochs, alpha = findBestHyperparameters(trainX, trainY, valX, valY)
+    print("bestNumUnitsInHiddenLayer",bestNumUnitsInHiddenLayer)
+    print("bestEpsilon", bestEpsilon)
+    print("bestMiniBatchSize", bestMiniBatchSize)
+    print("bestNumEpochs", bestNumEpochs)
+    print("alpha", alpha)
+    
 
+    # trainedW = train(trainX,trainY, w, bestEpsilon, bestMiniBatchSize, bestNumEpochs, alpha)
+    trainedW = train(trainX,trainY, bestNumUnitsInHiddenLayer, bestEpsilon, bestMiniBatchSize, bestNumEpochs, alpha)
 
+    testYHat = getYHat(testX, trainedW, bestNumUnitsInHiddenLayer )
+    print("Test Accuracy:",fPC(testY,testYHat)*100)
     # Train the network using SGD.
-    # train(trainX, trainY, testX, testY, w)
+    # print(train(trainX, trainY, w, 0.01, 128, 10 ,0.01).shape)
+    
+    
